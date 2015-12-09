656a0b14a6e1b51253990be5f458dacba4144354
Klimov Anton <antonklimovv@gmail.com>
2015-12-09 13:30:07 +0200

    jslerning4.1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function message(m){
-alert (m);
+function areaOfTriangle(obj){
+var a=1*obj.st1.value;
+var b=1*obj.st2.value;
+var c=1*obj.st3.value;
+var p=(a+b+c)/2;
+var s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+s=s.toFixed(2);
+obj.res.value=s
 }
